3d52027c460b967efff10e043d67245c3ba0b6e3
Santroy <krytront@gmail.com>
2016-12-21 05:13:23 +0100

    Add project files.

---
+++ ControllerTask/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using ControllerTask.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControllerTask.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public string SendEmail(Message m)
+        {
+            return string.Format("Otrzymałeś wiadomość od {0}, temat: {1}, wiadomość: {2}.",
+                m.Email, m.Topic, m.Text);
+        }
+    }
+}
---
+++ ControllerTask/Models/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ControllerTask.Models
+{
+    public class Message
+    {
+        public string Email { get; set; }
+        public string Topic { get; set; }
+        public string Text { get; set; }
+    }
+}
